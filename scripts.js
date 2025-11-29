a9c96c441dc018c015d14a20c89c69e261dc2d69
Dhii Santos <diegoalphapro@gmail.com>
2025-11-29 17:40:38 -0300

    Reajustes 40%

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 const buttonMenu = document.querySelector('.button-lateral')
 const menuLateral = document.querySelector('.menu-lateral')
 const ul = document.querySelector('ul')
+const background = document.querySelector('.background')
+const body = document.querySelector('body')
 
 
 
@@ -9,4 +11,15 @@ buttonMenu.addEventListener('click', () => {
     buttonMenu.classList.toggle('active')
     menuLateral.classList.toggle('active')
     ul.classList.toggle('active')
+    background.classList.toggle('active')
+    body.classList.toggle('active')
+})
+
+background.addEventListener('click', () => {
+
+    buttonMenu.classList.remove('active')
+    menuLateral.classList.remove('active')
+    ul.classList.remove('active')
+    background.classList.remove('active')
+    body.classList.remove('active')
 })
